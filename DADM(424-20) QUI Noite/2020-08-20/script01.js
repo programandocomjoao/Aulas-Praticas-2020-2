83013619930432ccf105e83071f899419e0bec7c
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2020-08-21 08:26:42 -0300

    Add files via upload

--- DADM(424-20) QUI Noite/2020-08-20/script01.js
+++ DADM(424-20) QUI Noite/2020-08-20/script01.js
@@ -1,1 +1,11 @@
+var http = require('http');
 
+http.createServer(
+    function(req, res) {
+        res.writeHead(200, {'Content-Type':'text/plain; charset=UTF-8'});
+        res.write('Desenvolvimento de Aplicações Distribuídas e Móveis');
+        res.end();
+    }
+).listen(8080);
+
+console.log('Servidor iniciado na porta 8080. Pressione Ctrl + C para encerrar.');
