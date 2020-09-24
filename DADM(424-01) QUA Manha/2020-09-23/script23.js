038fd54a84aecd2489c880c587f1df4deb4362e1
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2020-09-24 10:55:50 -0300

    Add files via upload

--- DADM(424-01) QUA Manha/2020-09-23/script23.js
+++ DADM(424-01) QUA Manha/2020-09-23/script23.js
@@ -1,1 +1,16 @@
+var http = require('http');
+var fs = require('fs');
 
+http.createServer(
+    function(req, res) {
+        fs.readFile('cadastro23.html',
+            function(err, pagina) {
+                res.writeHead(200, {'Content-Type':'text/html; charset=UTF-8'});
+                res.write(pagina);
+                res.end();
+            }
+        );
+    }
+).listen(8080);
+
+console.log('Servidor iniciado na porta 8080. Pressione Ctrl + C para encerrar.');
