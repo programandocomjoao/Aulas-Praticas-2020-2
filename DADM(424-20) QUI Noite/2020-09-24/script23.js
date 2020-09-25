bb3e5daae5733b4fceaee808c668a177a6ed3cf7
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2020-09-25 11:55:04 -0300

    Add files via upload

---
+++ DADM(424-20) QUI Noite/2020-09-24/script23.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var fs = require('fs');
+var formidable = require('formidable');
+
+http.createServer(
+    function(req, res) {
+        if (req.url == '/script23') {
+            var form = new formidable.IncomingForm();
+
+            form.parse(req, 
+                function(err, fields, files) {
+                    var arquivoAtual = files.foto.path;
+                    var arquivoNovo = 'C:/Users/jpvoi/Documents/DADM/' + files.foto.name;
+
+                    fs.rename(arquivoAtual, arquivoNovo, 
+                        function(err) {
+                            if (err) 
+                                throw err;
+                        }
+                    );
+
+                    res.writeHead(200, {'Content-Type':'text/html; charset=UTF-8'});
+                    res.write('<h1>Dados do Animal Cadastrado</h1>');
+                    res.write('<p>Nome: ' + fields.nome + '</p>');
+                    res.write('<p>Ano de nascimento: ' + fields.ano + '</p>');
+                    res.write('<p>Espécie: ' + fields.especie + '</p>');
+                    res.write('<p>Raça: ' + fields.raca + '</p>');
+                    res.end();
+                }
+            );
+        }
+        else {
+            fs.readFile('cadastro23.html',
+                function(err, pagina) {
+                    res.writeHead(200, {'Content-Type':'text/html; charset=UTF-8'});
+                    res.write(pagina);
+                    res.end();
+                }
+            );
+        }
+    }
+).listen(8080);
+
+console.log('Servidor iniciado na porta 8080. Pressione Ctrl + C para encerrar.');
