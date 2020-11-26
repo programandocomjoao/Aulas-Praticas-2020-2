70c40e144d9f9ac544121fda372157930d709da1
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2020-11-26 10:33:42 -0300

    Add files via upload

--- DADM(424-01) QUA Manha/2020-11-25/App.js
+++ DADM(424-01) QUA Manha/2020-11-25/App.js
@@ -1,1 +1,21 @@
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeApp from './componentes/home';
+import MidiasApp from './componentes/midias';
 
+const Stack = createStackNavigator();
+
+function UnigranrioApp() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeApp} />
+        <Stack.Screen name="Mídias Sociais" component={MidiasApp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default UnigranrioApp;
