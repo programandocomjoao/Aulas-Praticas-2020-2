fbc2fb815f801e72709b2e02f2521310c9d2c7c6
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2020-11-27 15:13:19 -0300

    Add files via upload

--- DADM(424-20) QUI Noite/2020-11-26/projetoUni/componentes/home.js
+++ DADM(424-20) QUI Noite/2020-11-26/projetoUni/componentes/home.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { View, Text, Image, Button } from 'react-native';
 
+const HomeApp = ({ navigation }) => {
+    return(
+      <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+          <Image
+            style={{width: 300, height: 49, marginTop: 30}}
+            source={require('../imagens/logoUnigranrio.png')}
+          />
+
+          <Text style={{padding: 20}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur iaculis tristique dui in gravida. Integer id lorem nec odio iaculis eleifend. Suspendisse potenti. Mauris et lectus condimentum, euismod mi sit amet, mattis purus. Praesent lacinia dapibus vestibulum. Cras accumsan semper magna vel semper. In interdum tortor porta metus ornare vehicula. Aenean tellus nulla, scelerisque at mollis in, volutpat vel urna. Aliquam tincidunt erat nisl, eget accumsan quam tempus non.</Text>
+
+          <Button 
+            title="Mídias Sociais"
+            onPress={() => navigation.navigate('Mídias Sociais')}
+          />
+      </View>
+    )
+  }
+  
+  export default HomeApp;
---
+++ DADM(424-20) QUI Noite/2020-11-26/projetoUni/componentes/midias.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { View, Text, Image, Button, Linking } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const MidiasApp = () => {
+    return(
+      <View style={{flex: 1, marginTop: 20}}>
+          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+                style={{width: 300, height: 49, marginTop: 30}}
+                source={require('../imagens/logoUnigranrio.png')}
+            />
+          </View>
+
+          <View style={{flex: 3, flexDirection: 'row', alignItems: 'stretch'}}>
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <TouchableOpacity onPress={() => {
+                    Linking.openURL('https://portal.unigranrio.edu.br');
+                }}>
+                    <Image 
+                        style={{width: 75, height: 75}}
+                        source={require('../imagens/iconeSite.png')}
+                    />
+                </TouchableOpacity>
+            </View>
+
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <TouchableOpacity onPress={() => {
+                    //Linking.openURL('https://www.google.com.br/maps');
+                    Linking.openURL('https://maps.app.goo.gl/F55oFKTYJjA1aCib6');
+                }}>
+                    <Image 
+                        style={{width: 75, height: 75}}
+                        source={require('../imagens/iconeMaps.png')}
+                    />
+                </TouchableOpacity>
+            </View>
+          </View>
+
+          <View style={{flex: 3, flexDirection: 'row', alignItems: 'stretch'}}>
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <TouchableOpacity onPress={() => {
+                    //Linking.openURL('mailto:');
+                    Linking.openURL('mailto:atendimento@unigranrio.com.br');
+                }}>
+                    <Image 
+                        style={{width: 75, height: 75}}
+                        source={require('../imagens/iconeEmail.png')}
+                    />
+                </TouchableOpacity>
+            </View>
+          
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <TouchableOpacity onPress={() => {
+                    Linking.openURL('http://api.whatsapp.com/send?phone=552132194040');
+                    //Linking.openURL('http://api.whatsapp.com/send?phone=552132194040&text=A aula de DADM é a melhor da semana');
+                }}>
+                    <Image 
+                        style={{width: 75, height: 75}}
+                        source={require('../imagens/iconeWhatsapp.png')}
+                    />
+                </TouchableOpacity>
+            </View>
+          </View>
+          
+          <View style={{flex: 3, flexDirection: 'row', alignItems: 'stretch'}}>
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <TouchableOpacity onPress={() => {
+                    //Linking.openURL('http://instagram.com');
+                    Linking.openURL('http://instagram.com/unigranrio');
+                }}>
+                    <Image 
+                        style={{width: 75, height: 75}}
+                        source={require('../imagens/iconeInstagram.png')}
+                    />
+                </TouchableOpacity>
+            </View>
+            
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <TouchableOpacity onPress={() => {
+                    //Linking.openURL('fb://');
+                    Linking.openURL('fb://page/111800282236339');
+                }}>
+                    <Image 
+                        style={{width: 75, height: 75}}
+                        source={require('../imagens/iconeFacebook.png')}
+                    />
+                </TouchableOpacity>
+            </View>
+          </View>
+      </View>
+    )
+  }
+  
+  export default MidiasApp;
